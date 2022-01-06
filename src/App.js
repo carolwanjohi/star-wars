b038bfddbb8f40e747d07d3e9512264c2cd346d5
Carol Wanjohi <wanjohi.carol54@gmail.com>
2022-01-06 20:43:01 +0300

    [WIP] create dummy form field and form using react-select package

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import SearchDropdown from './components/search-dropdown/SearchDropdown';
+
+const list = [
+  { id: 1, data: 'Shark' },
+  { id: 2, data: 'Dolphin' },
+  { id: 3, data: 'Whale' },
+  { id: 4, data: 'Octopus' },
+  { id: 5, data: 'Crab' },
+  { id: 6, data: 'Lobster' },
+  { id: 5, data: 'Cow' },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <SearchDropdown list={list} />
     </div>
   );
 }
